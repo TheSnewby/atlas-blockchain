e1aed231542cc3bd4f2aa7c0478b73718a5997e7
TheSnewby <stephen.j.newby@gmail.com>
2025-09-20 17:02:13 -0500

    ec_verify using ECDSA_verify

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -13,5 +13,11 @@
 int ec_verify(EC_KEY const *key, uint8_t const *msg,
 	size_t msglen, sig_t const *sig)
 {
-	
+	if (!key || !msg || !sig)
+		return (0);
+
+	if (!ECDSA_verify(0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key))
+		return (0);
+
+	return (1);
 }
