9a75ee74cb50a59d1df9dc35086285e2302dd8ee
TheSnewby <stephen.j.newby@gmail.com>
2025-09-30 17:22:46 -0500

    betty

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -10,6 +10,8 @@ blockchain_t *blockchain_create(void)
 	blockchain_t *newBC = NULL;
 	llist_t *newLL = NULL;
 	block_t *newB = NULL;
+	char *holberton = "Holberton School";
+	int data_len = strlen(holberton);
 
 	/* Create Initial Block */
 	newB = (block_t *)malloc(sizeof(block_t));
@@ -21,8 +23,8 @@ blockchain_t *blockchain_create(void)
 	newB->info.timestamp = 1537578000;
 	newB->info.nonce = 0;
 	memset(newB->info.prev_hash, 0, 32);
-	memcpy(newB->data.buffer, "Holberton School", strlen("Holberton School"));
-	newB->data.len = 16;
+	memcpy(newB->data.buffer, holberton, data_len);
+	newB->data.len = data_len;
 	memcpy(newB->hash, HOLBERTON_HASH, SHA256_DIGEST_LENGTH);
 
 	/* Create Blockchain */
