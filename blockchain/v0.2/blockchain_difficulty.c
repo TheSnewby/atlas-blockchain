fdd14c8592d5f0d3d321d4a9825b7b0de6b35875
TheSnewby <stephen.j.newby@gmail.com>
2025-10-13 23:36:35 -0500

    WIP

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -8,7 +8,7 @@
  */
 uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
-	uint32_t next_diff;
+	uint32_t next_difficulty;
 	block_t *last_block, *last_changed_block;
 	uint64_t expected_time_elapsed, actual_time_elapsed;
 
@@ -16,26 +16,29 @@ uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 		return (0);
 
 	last_block = (block_t *)llist_get_tail(blockchain->chain);
-	next_diff = last_block->info.difficulty;
+	next_difficulty = last_block->info.difficulty;
 
-	if (last_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0 &&
-		last_block->info.index != 0)
+	/* if index at adjustment interval */
+	if ((last_block->info.index % DIFFICULTY_ADJUSTMENT_INTERVAL == 0) &&
+		(last_block->info.index != 0))
 	{
+		/* set last time a block was up for changing */
 		last_changed_block = (block_t *)llist_get_node_at(blockchain->chain,
 			last_block->info.index - DIFFICULTY_ADJUSTMENT_INTERVAL);
+		/* find expected and actual time elapsed */
 		expected_time_elapsed = BLOCK_GENERATION_INTERVAL *
 		DIFFICULTY_ADJUSTMENT_INTERVAL;
 		actual_time_elapsed = last_block->info.timestamp -
 		last_changed_block->info.timestamp;
 
-		/* Too Fast/Easy */
+		/* Too Fast/Easy Case */
 		if (actual_time_elapsed < (expected_time_elapsed / 2))
-			return (++next_diff);
-		/* Too Slow/Difficult */
+			return (++next_difficulty);
+		/* Too Slow/Difficult Case */
 		else if ((actual_time_elapsed > (expected_time_elapsed * 2)) &&
-		(next_diff != 0))
-			return (--next_diff);
+		(last_block->info.difficulty != 0))
+			return (--next_difficulty);
 	}
 
-	return (last_block->info.difficulty);
+	return (next_difficulty);
 }
