6aa88096c1ac1b15ff02c924f82c9bb25a498b8d
TheSnewby <stephen.j.newby@gmail.com>
2025-10-13 23:22:50 -0500

    adjusted multiple aspects

--- blockchain/v0.2/blockchain_difficulty.c
+++ blockchain/v0.2/blockchain_difficulty.c
@@ -8,7 +8,7 @@
  */
 uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 {
-	uint32_t next_diff = 0;
+	uint32_t next_diff;
 	block_t *last_block, *last_changed_block;
 	uint64_t expected_time_elapsed, actual_time_elapsed;
 
@@ -22,15 +22,19 @@ uint32_t blockchain_difficulty(blockchain_t const *blockchain)
 		last_block->info.index != 0)
 	{
 		last_changed_block = (block_t *)llist_get_node_at(blockchain->chain,
-			last_block->info.index - 5);
-		expected_time_elapsed = 5 * BLOCK_GENERATION_INTERVAL;
+			last_block->info.index - DIFFICULTY_ADJUSTMENT_INTERVAL);
+		expected_time_elapsed = BLOCK_GENERATION_INTERVAL *
+		DIFFICULTY_ADJUSTMENT_INTERVAL;
 		actual_time_elapsed = last_block->info.timestamp -
 		last_changed_block->info.timestamp;
 
-		if ((actual_time_elapsed / 2) > expected_time_elapsed)
-			return (--next_diff);
-		else if ((actual_time_elapsed * 2) < expected_time_elapsed)
+		/* Too Fast/Easy */
+		if (actual_time_elapsed < (expected_time_elapsed / 2))
 			return (++next_diff);
+		/* Too Slow/Difficult */
+		else if ((actual_time_elapsed > (expected_time_elapsed * 2)) &&
+		(next_diff != 0))
+			return (--next_diff);
 	}
 
 	return (last_block->info.difficulty);
