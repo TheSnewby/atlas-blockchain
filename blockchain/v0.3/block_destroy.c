66c8a3d14c06155c0b949d6b9b7e66432bce0a73
TheSnewby <stephen.j.newby@gmail.com>
2025-11-03 14:51:41 -0600

    added a & to the llist-destroy call

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -9,7 +9,7 @@ void block_destroy(block_t *block)
 	if (!block)
 		return;
 
-	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
+	llist_destroy(block->transactions, 1, (node_dtor_t)&transaction_destroy);
 
 	free(block);
 	block = NULL;
