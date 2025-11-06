ba5a0dda479d2bda8a35ae911bf716827489f006
TheSnewby <stephen.j.newby@gmail.com>
2025-11-06 13:27:22 -0600

    added many debug messages

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -14,20 +14,33 @@ uint8_t *block_hash(block_t const *block,
 	unsigned char *buffer = NULL;
 
 	if (!block || !hash_buf)
+	{
+		fprintf(stderr, "!param\n");
 		return (NULL);
+	}
 
 	buffer = (unsigned char *)malloc(block->data.len + sizeof(block->info) +
 	transactions_size);
+	if (!buffer)
+	{
+		fprintf(stderr, "!buffer\n");
+		return (NULL);
+	}
 	memcpy(buffer, (unsigned char *)block,
 	sizeof(block->info) + block->data.len);
+	fprintf(stderr, "1\n");
 	memcpy(buffer + sizeof(block->info) + block->data.len,
 	((transaction_t *)block->transactions)->inputs, sizeof(tx_in_t));
+	fprintf(stderr, "2\n");
 	memcpy(buffer + sizeof(block->info) + block->data.len + sizeof(tx_in_t),
 	((transaction_t *)block->transactions)->outputs, sizeof(tx_out_t));
+	fprintf(stderr, "3\n");
 
 	SHA256(buffer, block->data.len + sizeof(block->info) +
 	transactions_size, hash_buf);
+	fprintf(stderr, "4\n");
 
 	free(buffer);
+	fprintf(stderr, "5\n");
 	return (hash_buf);
 }
