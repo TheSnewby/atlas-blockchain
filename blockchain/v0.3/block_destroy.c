3a490e98f7a2778a05d01dd4166c42259ea86848
TheSnewby <stephen.j.newby@gmail.com>
2025-11-10 23:40:26 -0600

    reverted

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -9,11 +9,8 @@ void block_destroy(block_t *block)
 	if (!block)
 		return;
 
-	if (llist_size(block->transactions) > 0)
+	if (block->transactions)
 		llist_destroy(block->transactions, 1,
-			(node_dtor_t)&transaction_destroy);
-	else
-		llist_destroy(block->transactions, 0,
 			(node_dtor_t)&transaction_destroy);
 
 	free(block);
