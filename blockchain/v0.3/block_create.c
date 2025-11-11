29ac0dcfe904ceb3a98d6448f94a04087774f409
TheSnewby <stephen.j.newby@gmail.com>
2025-11-11 00:02:52 -0600

    memsetting the new_block

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -15,7 +15,7 @@ block_t *block_create(block_t const *prev,
 	block_t *new_block = NULL;
 	unsigned int true_data_len;
 
-	if (!prev || !data || data_len == 0)
+	if (!prev || !data)
 		return (NULL);
 
 	true_data_len = (data_len <= BLOCKCHAIN_DATA_MAX) ?
@@ -25,6 +25,7 @@ block_t *block_create(block_t const *prev,
 	if (!new_block)
 		return (NULL);
 
+	memset(new_block, 0, sizeof(block_t));
 	memcpy(new_block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	new_block->info.index = prev->info.index + 1;
 	new_block->info.difficulty = 0;
